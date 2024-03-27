4b948155f98daaad9692d90521f6ee38db2c7116
freddy-reckling <freddy.reckling@gmail.com>
2024-03-26 22:27:58 -0500

    modify server.js file to serve the HTML file directly

--- server.js
+++ server.js
@@ -1,12 +1,12 @@
 const express = require('express');
 const app = express();
-const port = 3000; // Choose any available port
+const port = 3000;
 
-
-app.use(express.static('simple-weather-app'));
-
+// Serve your HTML file directly
+app.get('/', (req, res) => {
+    res.sendFile(__dirname + '/index.html');
+});
 
 app.listen(port, () => {
     console.log(`Server is running on http://localhost:${port}`);
 });
-
